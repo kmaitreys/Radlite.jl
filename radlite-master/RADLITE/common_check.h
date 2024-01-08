2341ca2f38e2930b4f7cd9eade1b07b053bdd7e3
kmaitreys <peakcipher1729@gmail.com>
2024-01-08 20:51:43 +0530

    moved to mirror

--- radlite-master/RADLITE/common_check.h
+++
@@ -1,6 +0,0 @@
-c     --------------------------------------------------------------
-c                     SELF-CHECK INFORMATION
-c     --------------------------------------------------------------
-      common/selfcheck/selfch_taumax
-      doubleprecision selfch_taumax
-c
